e2c6fa953a9ca3eae4977948273e04a343642a3a
Dhvan <34456470+Dh-Van@users.noreply.github.com>
2024-03-13 17:39:05 +0000

    start of some code

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
+// Parse JSON bodies
 app.use(express.json());
+
+// Serve static files from the "express" directory
 app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
+
+// Default URL for the website
+app.use('/', function (req, res) {
+	// __dirname: It will resolve to your project folder.
+	res.sendFile(path.join(__dirname + '/express/index.html'));
+});
+
 const server = http.createServer(app);
 const port = 3000;
+
 server.listen(port);
 console.debug('Server listening on port ' + port);
